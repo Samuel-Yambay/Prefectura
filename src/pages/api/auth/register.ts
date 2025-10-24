168bbbcd52158205dd20fc5a3e170770342628eb
Samuel-Yambay <bayronyambay2000@gmail.com>
2025-10-24 09:27:31 -0500

    fix: import supabase in register api

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,18 +1,21 @@
 import type { APIRoute } from 'astro';
-//import { supabase } from '../../../lib/supabase';
-//import { something } from "@/lib/supabase";
+import { supabase } from '../../../lib/supabaseClient';
 
+export const prerender = false;
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
-  const email = formData.get('email')?.toString();
-  const password = formData.get('password')?.toString();
+  const email = String(formData.get("email") ?? "");
+  const password = String(formData.get("password") ?? "");
 
   if (!email || !password) {
-    return new Response('Email and password are required', { status: 400 });
+    return new Response("Email y password son obligatorios", { status: 400 });
   }
 
-  const { error } = await supabase.auth.signUp({ email, password });
+  const { error } = await supabase.auth.signUp({
+    email,
+    password
+  });
 
   if (error) {
     return new Response(error.message, { status: 500 });
@@ -20,3 +23,4 @@ export const POST: APIRoute = async ({ request, redirect }) => {
 
   return redirect('/signin');
 };
+
