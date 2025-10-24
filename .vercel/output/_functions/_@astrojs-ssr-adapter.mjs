3ee04386f1f892f4f638d1a2a89972e5314afdce
Samuel-Yambay <bayronyambay2000@gmail.com>
2025-10-24 10:36:23 -0500

    chore: ignore .vercel and dist

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_BPsi8xx1.mjs';
--- .vercel/output/_functions/chunks/supabaseClient_CFew0sUR.mjs
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  "https://ojdchphtsehygqsolyrc.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qZGNocGh0c2VoeWdxc29seXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0ODA2NzYsImV4cCI6MjA1NzA1NjY3Nn0.6BTE_o9L9uItr6VzxPWB3Lz1EmV9_Z_oGjCTbWELt9Q"
-);
-
-export { supabase as s };
