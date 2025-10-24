ab61c12d84ec0ffc70dfbaf780863b11515ed359
Samuel-Yambay <bayronyambay2000@gmail.com>
2025-10-24 09:41:07 -0500

    chore(astro): use vercel serverless adapter (output=server)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-      output: 'server',
-      adapter: vercel(),
-     //site: process.env.MY_SITE_URL || 'http://localhost:4321',
+  output: 'server',
+  adapter: vercel(),
 });
-// Puedes renombrarlos como:
-
-
 
